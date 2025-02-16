e7a8127ee1a41ae742fe75eb5fc64d89c65159cb
fer <lfrecalde1@espe.edu.ec>
2025-02-15 20:01:54 -0500

    Final Code

--- Wrapper.py
+++ Wrapper.py
@@ -4,8 +4,8 @@ import argparse
 import matplotlib.pyplot as plt
 import casadi as ca
 from functions.functions import *
-from scipy.spatial.transform import Rotation
 import time
+import cv2 as cv
 
 
 def main():
@@ -36,18 +36,44 @@ def main():
     image_files = GetImagesNames(base_path)
 
     # Compute Corners and point over the cheesboard plane
-    data_uv, data_xy = ComputeCorners(base_path, pattern_size, image_files)
+    data_uv, data_xy, objpoints, imgpoints, gray, corners_image = ComputeCorners(
+        base_path, pattern_size, image_files
+    )
 
     U = data_uv
     X = data_xy
+
+    # Computing Homography
     H = EstimateHomography(X, U)
-    A = EstimateBMatrix((H))
+
+    # Computing intrinsic parameters
+    A = EstimateBMatrix(H)
+
+    # Computing Extrinsic parameters
     R, t = EstimatePose(H, A)
+
+    # Computing Radial Distortion
     d = EstimateDistortion(X, U, R, t, A)
-    x_init = SetInitialConditions(R, t, A)
+
+    # Set vector of initial conditions for the optimizer considering the stereographic projection
+    x_init = SetInitialConditions(R, t, A, d)
+    x_init = x_init.reshape((x_init.shape[0], 1))
+    A_vector = np.array(
+        [
+            A[0, 0],
+            A[0, 1],
+            A[0, 2],
+            A[1, 0],
+            A[1, 1],
+            A[1, 2],
+            A[2, 0],
+            A[2, 1],
+            A[2, 2],
+        ]
+    )
 
     # Reshape data
-    pts1_dm = X[0:2, :, 0]  # Start with the first slice, shape: (2, 54)
+    pts1_dm = X[0:2, :, 0]
     for i in range(1, X.shape[2]):
         pts1_dm = np.hstack((pts1_dm, X[0:2, :, i]))
 
@@ -56,21 +82,66 @@ def main():
     for i in range(1, U.shape[2]):
         pts2_dm = np.hstack((pts2_dm, U[0:2, :, i]))
 
-    # Create Optimization
-    opti, a_vector = cameraCalibrationOpti(2, X.shape[1], X.shape[2], pts1_dm, pts2_dm)
-    opti.set_initial(a_vector, x_init)
-    tic = time.time()
-    sol = opti.solve()
-    toc_libray = time.time() - tic
-    print("Solution found in", toc_libray, "seconds")
-
+    # Casadi Function to compute the cost
     f_casadi = cameraCalibrationCostFunction(2, X.shape[1], X.shape[2])
+    cost = CostFunction(2, X.shape[1], X.shape[2])
+    cost_classic = CostFunctionClassic(2, X.shape[1], X.shape[2])
+
+    # Optimization problem
     solver = cameraCalibrationCasADi(X[0:2, :, :], U[0:2, :, :], f_casadi)
+
+    # Get time to find the solution of the problem
     tic = time.time()
     sol = solver(x0=x_init)
     x_opt = sol["x"]
+    x_opt = np.array(x_opt)
     toc = time.time() - tic
-    print(toc, toc_libray)
+
+    # getting parameters from the optimzed vector
+    A_opti, R_opti, t_opti, d_opti = GetOptiParameters(x_opt, X.shape[2])
+
+    # print cost bafore optimization and afer it
+    cost_optimization = cost(x_opt, pts1_dm, pts2_dm)
+    cost_value_classic = CostFunctionMatrix(A, R, t, d, X[0:2, :, :], U[0:2, :, :])
+
+    # checking the results of the optimizer
+    # print("Rotation parameters")
+    # print("Initial estimation")
+    # print(R[:, :, 1])
+    # print("Optimization")
+    # print(R_opti[:, :, 1])
+
+    # print("Translation parameters")
+    # print("Initial estimation")
+    # print(t[:, 1])
+    # print("Optimization")
+    # print(t_opti[:, 1])
+
+    print("Intrinsic")
+    print("Initial estimation")
+    print(A)
+    print("Optimization")
+    print(A_opti)
+
+    print("Distortion")
+    print("Initial estimation")
+    print(d)
+    print("Optimization")
+    print(d_opti)
+
+    # Cost Values
+    print("Cost values before optimization")
+    print(cost_value_classic)
+    print("Cost values after optimization")
+    print(cost_optimization)
+
+    print("Time to solve the optimization problem")
+    print(toc)
+
+    # Check the results of the estimation
+    data = Results(base_path, image_files, A_opti, d_opti)
+    plot_banks(data[:, 0:6], "results", base_path)
+    plot_banks_detection(corners_image[:, 0:6], "results_corners", base_path)
 
 
 if __name__ == "__main__":
--- functions/functions.py
+++ functions/functions.py
@@ -5,6 +5,11 @@ import os
 from scipy.io import savemat
 import matplotlib.pyplot as plt
 import casadi as ca
+from scipy.spatial.transform import Rotation
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import matplotlib.patches as mpatches
 
 
 def GetImagesNames(base_path):
@@ -32,6 +37,54 @@ def GetCorners(base_path, image_files):
     return None
 
 
+def Results(base_path, image_files, camera_matrix, dist_coeffs):
+    # Loop to read the images
+    empty_matrix_pb = np.empty((2, len(image_files)), dtype=object)
+    for k, image_name in enumerate(image_files):
+        full_file_name = os.path.join(base_path, image_name)
+        print(f"Reading and processing image: {full_file_name}")
+
+        I = cv2.imread(full_file_name)
+        if I is None:
+            print(f"Failed to load image: {full_file_name}")
+            continue
+
+        # Convert to grayscale
+        gray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+        h, w = gray.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
+            camera_matrix, dist_coeffs, (w, h), 1, (w, h)
+        )
+        dst = cv2.undistort(gray, camera_matrix, dist_coeffs, None, newcameramtx)
+
+        corners = cv2.goodFeaturesToTrack(
+            dst, maxCorners=100, qualityLevel=0.01, minDistance=10
+        )
+
+        # If corners were found, draw them on the image
+        if corners is not None:
+            # Convert corner coordinates to integers
+            corners = np.int32(corners)
+            for corner in corners:
+                x_corner, y_corner = corner.ravel()
+                cv2.circle(
+                    dst,
+                    (x_corner, y_corner),
+                    radius=3,
+                    color=(255, 255, 255),
+                    thickness=20,
+                )
+
+        # crop the image
+        x, y, w, h = roi
+        dst = dst[y : y + h, x : x + w]
+
+        empty_matrix_pb[0, k] = gray
+        empty_matrix_pb[1, k] = dst
+
+    return empty_matrix_pb
+
+
 def ComputeCorners(base_path, pattern_size, image_files):
     # Size of the square inside the cheesboard
     nx = pattern_size[0] - 1
@@ -51,6 +104,17 @@ def ComputeCorners(base_path, pattern_size, image_files):
     data_uv = np.ones((3, points2D.shape[1], num_images))
     data_xy = np.ones((3, points2D.shape[1], num_images))
 
+    # Aux values to compare with baseline
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((nx * ny, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:6, 0:9].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    empty_matrix_pb = np.empty((1, len(image_files)), dtype=object)
+
     for k, image_name in enumerate(image_files):
         full_file_name = os.path.join(base_path, image_name)
         print(f"Reading and processing image: {full_file_name}")
@@ -67,7 +131,7 @@ def ComputeCorners(base_path, pattern_size, image_files):
         ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
 
         if ret:
-            drawn_image = I.copy()
+            drawn_image = gray.copy()
             cv2.drawChessboardCorners(drawn_image, pattern_size, corners, ret)
 
             image_points = corners.reshape(-1, 2).T
@@ -88,6 +152,11 @@ def ComputeCorners(base_path, pattern_size, image_files):
 
             # Save points model plane
             data_xy[0:2, :, k] = points2D[0:2, :]
+
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            empty_matrix_pb[0, k] = drawn_image
         else:
             print(f"Checkerboard not detected in image: {full_file_name}")
 
@@ -96,7 +165,7 @@ def ComputeCorners(base_path, pattern_size, image_files):
         {"data_uv_python": data_uv, "data_xy_python": data_xy},
     )
     print("Calibration data saved to calibration_values.mat")
-    return data_uv, data_xy
+    return data_uv, data_xy, objpoints, imgpoints, gray, empty_matrix_pb
 
 
 def Normalization(X):
@@ -381,6 +450,313 @@ def EstimateDistortion(X, U, R_estimation, t_estimation, A):
     return distortion
 
 
+def GetOptiParameters(a_vector, samples):
+    N = samples
+    # Intrinsic parameters
+    A = np.array(
+        [
+            [a_vector[0, 0], a_vector[1, 0], a_vector[3, 0]],
+            [0.0, a_vector[2, 0], a_vector[4, 0]],
+            [0, 0, 1],
+        ]
+    )
+
+    # Optimization variables related to the orientation and position
+    vector_optimization = a_vector[7:]
+    x_vector = ca.reshape(vector_optimization, 6, N)
+
+    x = x_vector[0:3, :]  # quaternions in a vector space
+    trans = x_vector[3:6, :]  # translations
+
+    R = np.zeros((3, 3, N))
+    t = np.zeros((3, N))
+    d = np.array([a_vector[5, 0], a_vector[6, 0], 0.0, 0.0, 0.0])
+
+    for k in range(N):
+        q_0 = (1 - x[0:3, k].T @ x[0:3, k]) / (1 + x[0:3, k].T @ x[0:3, k])
+        q_1 = 2 * x[0, k] / (1 + x[0:3, k].T @ x[0:3, k])
+        q_2 = 2 * x[1, k] / (1 + x[0:3, k].T @ x[0:3, k])
+        q_3 = 2 * x[2, k] / (1 + x[0:3, k].T @ x[0:3, k])
+
+        quaternion = np.array([float(q_0), float(q_1), float(q_2), float(q_3)])
+
+        t[:, k] = np.array([float(trans[0, k]), float(trans[1, k]), float(trans[2, k])])
+        R[:, :, k] = quat_to_rot(quaternion)
+
+    return A, R, t, d
+
+
+def CostFunctionClassic(n, points, samples):
+    # Dimensions
+    n_rows = n
+    n_cols = points
+    N = samples
+
+    # Number of data points total
+    n_pts_total = n_cols * N
+
+    # Numebr of optimization variables hard code values
+    n_params = 7 + 6 * N
+
+    # Define symbolic variables (where we have the parameters to optimize and the inputs such as points in the model plane and image plane)
+    a_vector = ca.SX.sym("full_estimation", n_params, 1)
+    A_aux = ca.SX.sym("A_aux", 9, 1)
+    pts1_sym = ca.SX.sym("pts1", n_rows, n_pts_total)
+    pts2_sym = ca.SX.sym("pts2", n_rows, n_pts_total)
+
+    # Intrinsic parameters
+    A = ca.vertcat(
+        ca.horzcat(A_aux[0], A_aux[1], A_aux[2]),
+        ca.horzcat(A_aux[3], A_aux[4], A_aux[5]),
+        ca.horzcat(A_aux[6], A_aux[7], A_aux[8]),
+    )
+
+    F_identity = ca.DM.eye(3)
+    Identity = ca.DM([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    const_transform = F_identity @ Identity
+
+    # Precompute
+    zeros_row = ca.DM.zeros(1, n_cols)
+    ones_row = ca.DM.ones(1, n_cols)
+    aux_last_element_homogeneous = ca.DM([[0, 0, 0, 1]])
+
+    # Optimization variables related to the orientation and position
+    vector_optimization = a_vector[7:]
+    x_vector = ca.reshape(vector_optimization, 6, N)
+    x = x_vector[0:3, :]  # quaternions in a vector space
+    trans = x_vector[3:6, :]  # translations
+
+    cost = 0
+    for k in range(N):
+        # Determine the column slice for the k-th sample (data stored as 2 x (points*N)).
+        start_index = k * n_cols
+        end_index = (k + 1) * n_cols
+
+        # Getting the values of the model plane and the image plane
+        pts1_slice = pts1_sym[:, start_index:end_index]  # shape: (n_rows x n_cols)
+        pts2_slice = pts2_sym[:, start_index:end_index]  # shape: (n_rows x n_cols)
+        U_real = pts2_slice
+
+        # Mapping the Euclidean space to quaternions
+        xk = x[:, k]  # 3x1 vector
+        norm_sq = ca.dot(xk, xk)
+        denom = 1 + norm_sq
+        q0 = (1 - norm_sq) / denom
+        q1 = 2 * xk[0] / denom
+        q2 = 2 * xk[1] / denom
+        q3 = 2 * xk[2] / denom
+        quaternion = ca.vertcat(q0, q1, q2, q3)
+
+        trans_aux = trans[:, k]
+
+        # Computing the rotation matrix and translation
+        R_est = quat_to_rot(quaternion)
+        T_estimated = ca.vertcat(
+            ca.horzcat(R_est, trans_aux), aux_last_element_homogeneous
+        )
+
+        # Create homogeneous coordinates for pts1_slice
+        homogeneous_pts = ca.vertcat(pts1_slice, zeros_row, ones_row)
+        values_normalized = const_transform @ T_estimated @ homogeneous_pts
+        aux_normalization = ca.vertcat(values_normalized[2, :], values_normalized[2, :])
+
+        # Normalized values
+        values_normalized_aux = values_normalized[0:2, :] / aux_normalization
+
+        # Computing Radial Distortion
+        radius = ca.sqrt(ca.sum1(values_normalized_aux**2))
+        D_expr = 1 + a_vector[5] * (radius**2) + a_vector[6] * (radius**4)
+        D_aux = ca.vertcat(D_expr, D_expr)
+        x_warp = values_normalized_aux * D_aux
+
+        # Create homogeneous coordinates for the warped points
+        x_warp_aux = ca.vertcat(x_warp, ones_row)
+        U_improved = A @ x_warp_aux
+        U_normalized_aux = ca.vertcat(U_improved[2, :], U_improved[2, :])
+        U_improved_final = U_improved[0:2, :] / U_normalized_aux
+
+        # Compute error M view
+        error = U_real - U_improved_final  # shape: (n_rows x n_cols)
+        cost = cost + ca.norm_fro(error)
+
+    # everthing in a vector
+    total_cost = cost / N
+
+    # Create the CasADi function.
+    f_cost = ca.Function(
+        "f_cost",
+        [a_vector, A_aux, pts1_sym, pts2_sym],
+        [total_cost],
+        ["a_vector", "A_aux", "pts1", "pts2"],
+        ["cost"],
+    )
+    return f_cost
+
+
+def CostFunction(n, points, samples):
+    # Dimensions
+    n_rows = n
+    n_cols = points
+    N = samples
+
+    # Number of data points total
+    n_pts_total = n_cols * N
+
+    # Numebr of optimization variables hard code values
+    n_params = 7 + 6 * N
+
+    # Define symbolic variables (where we have the parameters to optimize and the inputs such as points in the model plane and image plane)
+    a_vector = ca.SX.sym("full_estimation", n_params, 1)
+    pts1_sym = ca.SX.sym("pts1", n_rows, n_pts_total)
+    pts2_sym = ca.SX.sym("pts2", n_rows, n_pts_total)
+
+    # Intrinsic parameters
+    A = ca.vertcat(
+        ca.horzcat(a_vector[0], 0.0, a_vector[3]),
+        ca.horzcat(0, a_vector[2], a_vector[4]),
+        ca.horzcat(0, 0, 1),
+    )
+
+    F_identity = ca.DM.eye(3)
+    Identity = ca.DM([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    const_transform = F_identity @ Identity
+
+    # Precompute
+    zeros_row = ca.DM.zeros(1, n_cols)
+    ones_row = ca.DM.ones(1, n_cols)
+    aux_last_element_homogeneous = ca.DM([[0, 0, 0, 1]])
+
+    # Optimization variables related to the orientation and position
+    vector_optimization = a_vector[7:]
+    x_vector = ca.reshape(vector_optimization, 6, N)
+    x = x_vector[0:3, :]  # quaternions in a vector space
+    trans = x_vector[3:6, :]  # translations
+
+    cost = 0
+    for k in range(N):
+        # Determine the column slice for the k-th sample (data stored as 2 x (points*N)).
+        start_index = k * n_cols
+        end_index = (k + 1) * n_cols
+
+        # Getting the values of the model plane and the image plane
+        pts1_slice = pts1_sym[:, start_index:end_index]  # shape: (n_rows x n_cols)
+        pts2_slice = pts2_sym[:, start_index:end_index]  # shape: (n_rows x n_cols)
+        U_real = pts2_slice
+
+        # Mapping the Euclidean space to quaternions
+        xk = x[:, k]  # 3x1 vector
+        norm_sq = ca.dot(xk, xk)
+        denom = 1 + norm_sq
+        q0 = (1 - norm_sq) / denom
+        q1 = 2 * xk[0] / denom
+        q2 = 2 * xk[1] / denom
+        q3 = 2 * xk[2] / denom
+        quaternion = ca.vertcat(q0, q1, q2, q3)
+
+        trans_aux = trans[:, k]
+
+        # Computing the rotation matrix and translation
+        R_est = quat_to_rot(quaternion)
+        T_estimated = ca.vertcat(
+            ca.horzcat(R_est, trans_aux), aux_last_element_homogeneous
+        )
+
+        # Create homogeneous coordinates for pts1_slice
+        homogeneous_pts = ca.vertcat(pts1_slice, zeros_row, ones_row)
+        values_normalized = const_transform @ T_estimated @ homogeneous_pts
+        aux_normalization = ca.vertcat(values_normalized[2, :], values_normalized[2, :])
+
+        # Normalized values
+        values_normalized_aux = values_normalized[0:2, :] / aux_normalization
+
+        # Computing Radial Distortion
+        radius = ca.sqrt(ca.sum1(values_normalized_aux**2))
+        D_expr = 1 + a_vector[5] * (radius**2) + a_vector[6] * (radius**4)
+        D_aux = ca.vertcat(D_expr, D_expr)
+        x_warp = values_normalized_aux * D_aux
+
+        # Create homogeneous coordinates for the warped points
+        x_warp_aux = ca.vertcat(x_warp, ones_row)
+        U_improved = A @ x_warp_aux
+        U_normalized_aux = ca.vertcat(U_improved[2, :], U_improved[2, :])
+        U_improved_final = U_improved[0:2, :] / U_normalized_aux
+
+        # Compute error M view
+        error = U_real - U_improved_final  # shape: (n_rows x n_cols)
+        cost = cost + ca.norm_fro(error)
+
+    # everthing in a vector
+    total_cost = cost / N
+
+    # Create the CasADi function.
+    f_cost = ca.Function(
+        "f_cost",
+        [a_vector, pts1_sym, pts2_sym],
+        [total_cost],
+        ["a_vector", "pts1", "pts2"],
+        ["cost"],
+    )
+    return f_cost
+
+
+def CostFunctionMatrix(A, R, t, d, pts1, pts2):
+    # Dimensions
+    n_rows = pts1.shape[0]
+    n_cols = pts1.shape[1]
+    N = pts1.shape[2]
+
+    F_identity = np.eye(3)
+    Identity = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    const_transform = F_identity @ Identity
+
+    # Precompute
+    zeros_row = np.zeros((1, n_cols))
+    ones_row = np.ones((1, n_cols))
+    aux_last_element_homogeneous = np.array([[0, 0, 0, 1]])
+    cost = 0
+
+    for k in range(N):
+        # Getting the values of the model plane and the image plane
+        U_real = pts2[:, :, k]
+        trans_aux = t[:, k]
+
+        # Computing the rotation matrix and translation
+        R_est = R[:, :, k]
+        T_estimated = np.hstack((R_est, trans_aux.reshape((3, 1))))
+        T_estimated = np.vstack((T_estimated, aux_last_element_homogeneous))
+
+        # Create homogeneous coordinates for pts1_slice
+        homogeneous_pts = np.vstack((pts1[:, :, k], zeros_row, ones_row))
+        values_normalized = const_transform @ T_estimated @ homogeneous_pts
+        aux_normalization = np.vstack(
+            (values_normalized[2, :], values_normalized[2, :])
+        )
+
+        # Normalized values
+        values_normalized_aux = values_normalized[0:2, :] / values_normalized[2, :]
+
+        # Computing Radial Distortion
+        radius = np.linalg.norm(values_normalized_aux, axis=0)
+        D_expr = 1 + d[0] * (radius**2) + d[1] * (radius**4)
+        D_aux = np.vstack((D_expr, D_expr))
+        x_warp = values_normalized_aux * D_expr
+
+        # Create homogeneous coordinates for the warped points
+        x_warp_aux = np.vstack((x_warp, ones_row))
+        U_improved = A @ x_warp_aux
+        U_normalized_aux = np.vstack((U_improved[2, :], U_improved[2, :]))
+        U_improved_final = U_improved[0:2, :] / U_improved[2, :]
+
+        # Compute error M view
+        error = U_real - U_improved_final  # shape: (n_rows x n_cols)
+        cost = cost + np.linalg.norm(error, "fro")
+
+    # everthing in a vector
+    total_cost = cost / N
+
+    return total_cost
+
+
 def cameraCalibrationCostFunction(n, points, samples):
     # Dimensions
     n_rows = n
@@ -400,7 +776,7 @@ def cameraCalibrationCostFunction(n, points, samples):
 
     # Intrinsic parameters
     A = ca.vertcat(
-        ca.horzcat(a_vector[0], a_vector[1], a_vector[3]),
+        ca.horzcat(a_vector[0], 0.0, a_vector[3]),
         ca.horzcat(0, a_vector[2], a_vector[4]),
         ca.horzcat(0, 0, 1),
     )
@@ -554,21 +930,36 @@ def cameraCalibrationCasADi(pts1, pts2, f_casadi):
 
 def SetInitialConditions(R_estimation, t_estimation, A, d):
     # Reshape my rotation matrix in order to have matrices compatible with the library
-    R_matrices = np.transpose(R_estimation, (2, 0, 1))
-    rotations = Rotation.from_matrix(R_matrices)
+    R_aux = np.zeros((R_estimation.shape[2], 3, 3))
+    for k in range(R_estimation.shape[2]):
+        R_aux[k, :, :] = R_estimation[:, :, k]
+
+    # R_matrices = np.transpose(R_estimation, (2, 0, 1))
+    rotations = Rotation.from_matrix(R_aux)
     quat_scipy = rotations.as_quat()  # shape: (n_samples, 4)
 
     # Quaternions are in the following form w, x, y, z
     quaternion_estimated = np.column_stack(
         (quat_scipy[:, 3], quat_scipy[:, 0], quat_scipy[:, 1], quat_scipy[:, 2])
     )
-
+    negative_mask = quaternion_estimated[:, 0] < 0
+    quaternion_estimated[negative_mask] *= -1
+    distortion_1 = d[0, 0]
+    distortion_2 = d[1, 0]
     # Initial solution from the intrinsic parameters
-    X_init_list = [A[0, 0], A[0, 1], A[1, 1], A[0, 2], A[1, 2], d[0], d[1]]
+    X_init_list = [
+        A[0, 0],
+        A[0, 1],
+        A[1, 1],
+        A[0, 2],
+        A[1, 2],
+        float(distortion_1),
+        float(distortion_2),
+    ]
 
     # Loop through each sample
     for k in range(R_estimation.shape[2]):
-        x_quaternion = quaternion_estimated[k, 1:] / quaternion_estimated[k, 0]
+        x_quaternion = quaternion_estimated[k, 1:4] / quaternion_estimated[k, 0]
         X_init_list.extend(x_quaternion.tolist() + t_estimation[:, k].tolist())
 
     X_init = np.array(X_init_list)
@@ -670,3 +1061,79 @@ def cameraCalibrationOpti(n, points, samples, pts1_dm, pts2_dm):
     opti.solver("ipopt", {"expand": True})
 
     return opti, a_vector
+
+
+def plot_banks(bank, name, location, include_classical=True):
+    n, m = bank.shape
+
+    # Create a figure with subplots
+    fig, axs = plt.subplots(n, m, figsize=(1.5 * m, 2 * n))
+    axs = np.ravel(axs)
+
+    # Loop over each image in the bank
+    for i in range(n):
+        for j in range(m):
+            subplot_idx = i * m + j
+            ax = axs[subplot_idx]
+
+            # Plot the image in grayscale
+            im = ax.imshow(bank[i, j], cmap="gray", interpolation="none")
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+            # If this subplot is in the first column, set its y-axis label.
+            if j == 0:
+                if i == 0:
+                    ax.set_ylabel("Original Image", fontsize=8)
+                elif i == 1:
+                    ax.set_ylabel("Undistort Image", fontsize=8)
+
+    # Add a colorbar to the figure
+    fig.colorbar(im, ax=axs, orientation="vertical", fraction=0.02, pad=0.02)
+
+    # Save the figure as a PDF
+    file_path = os.path.join(location, f"{name}.pdf")
+    plt.savefig(file_path, format="pdf", bbox_inches="tight")
+    plt.clf()
+    plt.close()
+
+    print(f"PDF with colorbar saved to: {file_path}")
+
+    return None
+
+
+def plot_banks_detection(bank, name, location, include_classical=True):
+    n, m = bank.shape
+
+    # Create a figure with subplots
+    fig, axs = plt.subplots(n, m, figsize=(1.5 * m, 2 * n))
+    axs = np.ravel(axs)
+
+    # Loop over each image in the bank
+    for i in range(n):
+        for j in range(m):
+            subplot_idx = i * m + j
+            ax = axs[subplot_idx]
+
+            # Plot the image in grayscale
+            im = ax.imshow(bank[i, j], cmap="gray", interpolation="none")
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+            # If this subplot is in the first column, set its y-axis label.
+            if j == 0:
+                if i == 0:
+                    ax.set_ylabel("Original Image", fontsize=8)
+
+    # Add a colorbar to the figure
+    fig.colorbar(im, ax=axs, orientation="vertical", fraction=0.02, pad=0.02)
+
+    # Save the figure as a PDF
+    file_path = os.path.join(location, f"{name}.pdf")
+    plt.savefig(file_path, format="pdf", bbox_inches="tight")
+    plt.clf()
+    plt.close()
+
+    print(f"PDF with colorbar saved to: {file_path}")
+
+    return None
